9e767f99e07e4384c5f8d8148c39fb68822ee15c
Jesse Millar <hellojessemillar@gmail.com>
2015-11-11 14:15:58 -0700

    Initial commit

---
+++ onaplane.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"io/ioutil"
+	"os"
+	"unicode"
+)
+
+func check(e error) { // Check file IO calls for errors
+	if e != nil {
+		panic(e)
+	}
+}
+
+func main() {
+	data, err := ioutil.ReadFile(os.Args[1])
+	check(err)
+	content := []rune(string(data[:]))
+
+	for i := 0; i < len(content); i++ {
+		if content[i] == '_' {
+			content[i+1] = unicode.ToUpper(content[i+1])
+		}
+	}
+
+	err = ioutil.WriteFile(os.Args[1], []byte(string(content)), 0644)
+	check(err)
+}
